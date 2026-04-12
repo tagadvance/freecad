afb754f5bfb2a3671605e1180f71cf1e2a3724b3
Tag <tagadvance+github.com@gmail.com>
2026-04-11 20:19:24 -0600

    Add cad volume and remove xauth

--- freecad.sh
+++ freecad.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-XAUTH=/tmp/.docker.xauth
 XSOCK=/tmp/.X11-unix
 
 version=${1:-"1.0.0"}
@@ -15,34 +14,24 @@ docker buildx build --platform linux/amd64 \
              $here
 
 # preserve settings between runs
-mkdir -p "$HOME/.cache/FreeCAD" \
+mkdir -p "$HOME/cad" \
+         "$HOME/.cache/FreeCAD" \
          "$HOME/.cache/mesa_shader_cache" \
          "$HOME/.cache/mesa_shader_cache_db" \
          "$HOME/.config/FreeCAD" \
          "$HOME/.local/share/FreeCAD"
-
-# While xhost +local:docker is more restrictive than xhost +
-# (which allows anyone on the network to access your screen),
-# it still lowers the security of your host machine.
-# For better security, use an Xauthority token (.Xauthority)
-# to grant access only to a specific container rather than
-# the entire local Docker user.
-
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run --rm -it \
            --user $UID:$UID \
            --network host \
            --env DISPLAY=$DISPLAY \
            --env QT_X11_NO_MITSHM=1 \
-           --env XAUTHORITY=$XAUTH \
            --device=/dev/dri:/dev/dri \
            --volume $XSOCK:$XSOCK:ro \
+           --volume $HOME/cad:/home/freecad/host \
            --volume $HOME/.cache/FreeCAD:/home/freecad/.cache/FreeCAD \
            --volume $HOME/.cache/mesa_shader_cache:/home/freecad/.cache/mesa_shader_cache \
            --volume $HOME/.cache/mesa_shader_cache_db:/home/freecad/.cache/mesa_shader_cache_db \
            --volume $HOME/.config/FreeCAD:/home/freecad/.config/FreeCAD \
            --volume $HOME/.local/share/FreeCAD:/home/freecad/.local/share/FreeCAD \
            freecad:latest
-
-rm --force $XAUTH
