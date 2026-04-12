9442682973538e1ae1856441bd5b7b880948cc66
Tag <tagadvance+github.com@gmail.com>
2026-04-11 21:18:03 -0600

    Publish image to ghcr.io

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+registry='ghcr.io/tagadvance'
+image_name='freecad'
+version=${1:-"1.0.0"}
+here=$(dirname "$(realpath $0)")
+
+docker buildx build --platform linux/amd64 \
+             --build-arg HOST_UID=$(id -u) \
+             --build-arg HOST_GID=$(id -g) \
+             --build-arg VERSION=$version \
+             --tag "$image_name:$version" \
+             --tag "$image_name:latest" \
+             --tag "$registry/$image_name:$version" \
+             --tag "$registry/$image_name:latest" \
+             $here
+
+docker push --all-tags "$registry/$image_name"
+
--- freecad.sh
+++ freecad.sh
@@ -2,16 +2,8 @@
 
 XSOCK=/tmp/.X11-unix
 
-version=${1:-"1.0.0"}
-here=$(dirname "$(realpath $0)")
-
-docker buildx build --platform linux/amd64 \
-             --build-arg HOST_UID=$(id -u) \
-             --build-arg HOST_GID=$(id -g) \
-             --build-arg VERSION=$version \
-             --tag "freecad:$version" \
-             --tag 'freecad:latest' \
-             $here
+registry='ghcr.io/tagadvance'
+image_name='freecad'
 
 # preserve settings between runs
 mkdir -p "$HOME/cad" \
@@ -34,4 +26,4 @@ docker run --rm -it \
            --volume $HOME/.cache/mesa_shader_cache_db:/home/freecad/.cache/mesa_shader_cache_db \
            --volume $HOME/.config/FreeCAD:/home/freecad/.config/FreeCAD \
            --volume $HOME/.local/share/FreeCAD:/home/freecad/.local/share/FreeCAD \
-           freecad:latest
+           "$registry/$image_name:latest"
